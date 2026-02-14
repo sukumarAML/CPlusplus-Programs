3314b627fc6c736078301ddc089f8dc15cffe815
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-02-14 06:31:38 +0530

    Update display.cpp

--- Basic IO operations/display.cpp
+++ Basic IO operations/display.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl; // Displays text and moves to the next line
+    int number = 10;
+    std::cout << "The number is: " << number << std::endl; // Cascading operations
+    return 0;
+}
 
