56ec79d916401ef004880878ba70ed0ca1a75c98
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-02-17 08:43:15 +0530

    Update if loop.cpp

--- Looping statements/if loop.cpp
+++ Looping statements/if loop.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+#include <iomanip> // For setting precision
+
+using namespace std;
+
+int main() {
+  int age;
+
+  cout << "Enter your age: ";
+  cin >> age;
+
+  // 1. Simple if
+  if (age >= 18) {
+    cout << "You are an adult." << endl;
+  }
+
+  // 2. Simple if-else
+  if (age >= 18) {
+    cout << "You are an adult." << endl;
+  } else {
+    cout << "You are a minor." << endl;
+  }
+
+  // 3. If-else if-else
+  if (age >= 65) {
+    cout << "You are a senior citizen." << endl;
+  } else if (age >= 18) {
+    cout << "You are an adult." << endl;
+  } else if (age >= 13) {
+    cout << "You are a teenager." << endl;
+  } else {
+    cout << "You are a child." << endl;
+  }
+
+  // Demonstrating multiple conditions
+  int score = 85;
+  if (score >= 90) {
+      cout << "Grade: A" << endl;
+  } else if (score >= 80) {
+      cout << "Grade: B" << endl;
+  } else if (score >= 70) {
+      cout << "Grade: C" << endl;
+  } else {
+      cout << "Grade: D" << endl;
+  }
+
+  return 0;
+}
 
