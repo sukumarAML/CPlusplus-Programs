1523bd26e5cdb10a5931e4c998bae1ea379f45e7
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-02-17 09:13:41 +0530

    Update polymorphismsample.cpp

--- Polymorphism/polymorphismsample.cpp
+++ Polymorphism/polymorphismsample.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Base class
+class Animal {
+public:
+    virtual void makeSound() {
+        cout << "Generic animal sound" << endl;
+    }
+};
+
+// Derived classes
+class Dog : public Animal {
+public:
+    void makeSound() override {
+        cout << "Woof!" << endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void makeSound() override {
+        cout << "Meow!" << endl;
+    }
+};
+
+int main() {
+    Animal* animal1 = new Dog();
+    Animal* animal2 = new Cat();
+
+    animal1->makeSound(); // Calls Dog's makeSound()
+    animal2->makeSound(); // Calls Cat's makeSound()
+
+    delete animal1;
+    delete animal2;
+
+    return 0;
+}
 
