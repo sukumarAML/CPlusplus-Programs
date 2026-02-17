f3d2f329f7025864f76a5b5b0c7619a9d2ee9ba7
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-02-17 08:44:09 +0530

    Update dowhileloops.cpp

--- Looping statements/dowhileloops.cpp
+++ Looping statements/dowhileloops.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <iomanip> // For setting precision
+
+using namespace std;
+
+int main() {
+  // 1. Do-While Loop
+  int count = 5;
+  do {
+    cout << "Do-While Loop: Count = " << count << endl;
+    count++;
+  } while (count <= 10);
+
+  cout << "\nDo-While loop finished." << endl;
+
+  // 2. While Loop
+  int number = 1;
+  while (number <= 5) {
+    cout << "While Loop: Number = " << number << endl;
+    number++;
+  }
+
+  cout << "\nWhile loop finished." << endl;
+
+  // Example: Getting user input until a valid number is entered
+  int userInput;
+  cout << "\nEnter a positive number (or enter -1 to exit): ";
+  cin >> userInput;
+
+  while (userInput != -1) {
+    cout << "You entered: " << userInput << endl;
+    cout << "Enter another positive number (or -1 to exit): ";
+    cin >> userInput;
+  }
+
+  cout << "\nProgram exiting." << endl;
+
+  return 0;
+}
 
