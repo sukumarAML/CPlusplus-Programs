ba7fb89c937f28b87080f1eb02e4ee5f5d4569ea
sukumarAML <147479940+sukumarAML@users.noreply.github.com>
2026-02-17 10:06:50 +0530

    Update multiple inheritance.cpp

--- Inheritance/multiple inheritance.cpp
+++ Inheritance/multiple inheritance.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+
+class Animal {
+public:
+  void eat() {
+    std::cout << "Animal is eating." << std::endl;
+  }
+};
+
+class Swimmer : public Animal {
+public:
+  void swim() {
+    std::cout << "Swimmer is swimming." << std::endl;
+  }
+};
+
+class Diver : public Animal {
+public:
+  void dive() {
+    std::cout << "Diver is diving." << std::endl;
+  }
+};
+
+class DiverSwimmer : public Diver, public Swimmer {
+public:
+  void performActivity() {
+    eat();
+    swim();
+    dive();
+  }
+};
+
+int main() {
+  DiverSwimmer diverSwimmer;
+  diverSwimmer.performActivity(); // Calls all three methods (eat, swim, dive)
+
+  return 0;
+}
 
